af4a6de726fa7ed32903704b224a15cc221a2d32
robegr42 <robegr42@gmail.com>
2021-03-09 14:36:15 -0500

    fixed parser

--- instructions.py
+++ instructions.py
@@ -2,18 +2,18 @@ import abc
 
 class Instruction(metaclass=abc.ABCMeta):
 
-    def __init__(self, time, args):
+    def __init__(self, time):
         self.time = time
-        self.args = args
 
     @abc.abstractmethod
     def execute(self, net_sim):
+        pass
         
 
 class CreateHubIns(Instruction):
 
     def __init__(self, time, hub_name, ports):
-        super(Instruction, self).__init__(time, [hub_name, f'{ports}'])
+        super(Instruction, self).__init__(time)
         self.hub_name = hub_name
         self.ports = ports
 
@@ -21,13 +21,13 @@ class CreateHubIns(Instruction):
 class CreateHostIns(Instruction):
 
     def __init__(self, time, host_name):
-        super(Instruction, self).__init__(time, [host_name])
+        super(Instruction, self).__init__(time)
         self.host_name = host_name
 
 class ConnectIns(Instruction):
 
     def __init__(self, time, port1, port2):
-        super(Instruction, self).__init__(time,[port1, port2])
+        super(Instruction, self).__init__(time)
         self.port1 = port1
         self.port2 = port2
     
@@ -35,13 +35,13 @@ class ConnectIns(Instruction):
 class SendIns(Instruction):
     
     def __init__(self, time, host_name, data):
-        super(Instruction, self).__init__(time, [host_name, ])
+        super(Instruction, self).__init__(time)
         self.host_name = host_name
         self.data = data
 
 class DisconnectIns(Instruction):
 
     def __init__(self, time, port_name):
-        super(Instruction, self).__init__(time, [port_name])
+        super(Instruction, self).__init__(time)
         self.port_name = port_name
     
