67366d0365f645f8587deb8d8f0f91197693047f
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-09 12:55:02 -0500

    simulation basics

--- simulation.py
+++ simulation.py
@@ -2,27 +2,26 @@
 
 class NetSimulation():
 
-    def __init__(self, instructions, signal_time=10, output_path='.'):
-        self.instructions = instructions
+    def __init__(self, signal_time=10, output_path='.'):
+        self.instructions = []
         self.signal_time = signal_time
         self.output_path = output_path
         self.time = 0
         self.inst_index = 0
-        
+        self.pending_devices = []
 
-    def start(self):
+
+    def start(self, instructions):
+        self.instructions = instructions
         self.time = 0
-        self.inst_index = 0
         pass
 
     def update(self):
         t = self.time
 
         current_insts = []
-        while self.inst_index < len(self.instructions) and
-              t == self.instructions[self.inst_index].time:
-            current_insts.append(self.instructions[self.inst_index])
-            self.inst_index += 1            
+        while self.instructions and t == self.instructions[0].time:
+            current_insts.append(self.instructions.pop(0))           
 
         for instr in current_insts:
             instr.execute(self)
