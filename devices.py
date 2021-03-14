67314ce0c199af5ccaefab2f57a214ba8f7f4b01
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-14 12:50:19 -0400

    pretty logs

--- devices.py
+++ devices.py
@@ -39,14 +39,19 @@ class Device(metaclass=abc.ABCMeta):
     def connect(self, cable, port_name):
         pass
 
-    def log(self, time, msg):
-        log_msg = f'{time} {self.name} {msg}'
+    def log(self, time, msg, info):
+        log_msg = f'| {time: ^10} | {self.name: ^8} | {msg: ^10} | {info: <30} |'
         self.logs.append(log_msg)
         logging.info(log_msg)
 
     def save_log(self, path=''):
-        with open(path + f'{self.name}.txt', 'w+') as f:
-            f.write('\n'.join(self.logs))
+        with open(path + f'{self.name}.txt', 'w+') as file:
+            header = f'| {"Time (ms)": ^10} | {"Port":^8} | {"Action" :^10} | {"Info": ^30} |'
+            file.write(f'{"-" * len(header)}\n')
+            file.write(f'{header}\n')
+            file.write(f'{"-" * len(header)}\n')
+            file.write('\n'.join(self.logs))
+            file.write(f'{"-" * len(header)}\n')
 
 class Hub(Device):
 
@@ -72,11 +77,11 @@ class Hub(Device):
                 cable.set_value(val)
 
     def connect(self, cable: Cable, port_name: str):
-
         if self.ports[port_name] is not None:
             raise ValueError(f'Port {port_name} is currently in use.')
         
         self.ports[port_name] = cable
+
 
 class PC(Device):
     def __init__(self, name, signal_time):
@@ -137,7 +142,7 @@ class PC(Device):
 
             if not coll:
                 if self.send_time == 0:                
-                    self.log(self.sim_time, f'send {self.sending_bit}')
+                    self.log(self.sim_time, 'Send', f'{self.sending_bit}')
                 self.send_time += 1
                 if self.send_time == self.signal_time:
                     self.package_index += 1
@@ -155,13 +160,13 @@ class PC(Device):
             self.check_collision()
         elif self.time_connected % self.signal_time == 0 and \
            not self.is_sending:
-            self.log(self.sim_time, f'recieve {self.cable.value}')
+            self.log(self.sim_time, 'Recieve', f'{self.cable.value}')
 
     def check_collision(self):
         if self.is_sending and self.cable.value != self.sending_bit: #collision
             self.readjust_max_time_to_send()
             self.time_to_send = randint(0, self.max_time_to_send)
-            self.log(self.sim_time, f'Collision, waitting {self.time_to_send}ms to send')
+            self.log(self.sim_time, 'Collision', f'Waitting {self.time_to_send}ms to send')
             self.package_index = 0
             self.send_time = 0
             self.is_sending = False
