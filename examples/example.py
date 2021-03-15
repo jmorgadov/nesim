feb05e67a3fb92e422c5b67f637e4ab7d73e1009
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-15 02:31:09 -0400

    add example

---
+++ examples/example.py
@@ -0,0 +1,10 @@
+import nesim
+
+raw_instr = []
+
+with open('script.txt', 'r') as file:
+    raw_instr = file.readlines()
+
+instr = nesim.parse_instructions(raw_instr)
+sim = nesim.NetSimulation()
+sim.start(instr)
