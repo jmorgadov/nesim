c855f81453896ad2a0412855d50fdea0bcdeb9b3
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-15 00:17:37 -0400

    set porject toml

--- netsim/main.py
+++
@@ -1,7 +0,0 @@
-import netsim
-
-if __name__ == '__main__':
-    sim = netsim.NetSimulation()
-    with open('input.txt', 'r') as f:
-        instr = netsim.parse_instructions(f.readlines())
-        sim.start(instr)
