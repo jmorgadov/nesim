d0a64372e6d60f90ac79ff92561f6212f52bc3bb
Jorge Morgado <jorge.morgadov@gmail.com>
2021-03-29 02:12:41 -0400

    Add switch class

--- nesim/devices/switch.py
+++ nesim/devices/switch.py
@@ -1,7 +1,8 @@
+from typing import Dict
 from nesim.devices.send_receiver import SendReceiver
 from nesim.devices.cable import DuplexCableHead
 from nesim.devices.device import Device
-
+from nesim.devices.utils import from_bit_data_to_number
 
 class Switch(Device):
 
@@ -10,11 +11,43 @@ class Switch(Device):
         ports = {}
         for i in range(ports_count):
             ports[f'{name}_{i+1}'] = None
-
+        self.ports_buffer = [[] for _ in range(ports_count)]
+        self.mac_table: Dict[int, str] = {}
         super().__init__(name, ports)
 
-    def _create_send_receiver(self, cable_head: DuplexCableHead):
+    def broadcast(self, data):
+        for port in self.ports.values():
+            port.send(data)
+
+    def handle_buffer_data(self, port):
+        data = self.ports_buffer[port]
+
+        if len(data) < 40:
+            return
+
+        to_mac = from_bit_data_to_number(data[:16])
+        from_mac = from_bit_data_to_number(data[16:32])
+        size = from_bit_data_to_number(data[32:40]) * 8
+
+        if len(data) - 40 < size:
+            return
+
+        self.mac_table[from_mac] = self.port_name(port)
+
+        if to_mac in self.mac_table:
+            self.ports[self.mac_table[to_mac]].send(data)
+        else:
+            self.broadcast(data)
+        self.ports_buffer[port] = []
+
+
+    def receive_on_port(self, port, bit):
+        self.ports_buffer[port].append(bit)
+        self.handle_buffer_data(port)
+
+    def _create_send_receiver(self, port, cable_head: DuplexCableHead):
         sr = SendReceiver(self.signa_time, cable_head)
+        sr.on_receive.append(lambda bit : self.receive_on_port(port, bit))
         return sr
 
     def connect(self, cable_head: DuplexCableHead, port_name: str):
@@ -23,7 +56,8 @@ class Switch(Device):
             raise ValueError(f'Port {port_name} is currently in use.')
 
         if sr is None:
-            sr = self._create_send_receiver(cable_head)
+            port_num = list(self.ports.keys()).index(port_name)
+            sr = self._create_send_receiver(port_num, cable_head)
 
         self.ports[port_name] = sr
 
